33a824da97309eb7c131cced19794307090d22a2
kurupt <kurupt_on@proton.me>
2026-07-10 18:36:45 -0300

    Adiciona o script para verificar a presença do docker

---
+++ docker.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+printf "Verificando o docker.\n"
+sleep 1
+
+command -v docker &>/dev/null
+
+if [ $? -ne 0 ];then
+
+	printf "Docker ausente.\n"
+	sleep 1
+	printf "Adicionando Repositório.\n"
+	sleep 1
+
+	. /etc/os-release
+
+	apt-cache update &>/dev/null
+	apt-cache install curl ca-certificates -y &>/dev/null
+	install -m 0755 -d /etc/apt/keyrings
+	curl -fsSL https://download.docker.com/linux/${ID}/gpg -o /etc/apt/keyrings/docker.asc &>/dev/null
+	chmod a+r /etc/apt/keyrings/docker.asc
+	
+	cat > /etc/apt/sources.list.d/docker.sources << EOF
+Types: deb
+URIs: https://download.docker.com/linux/"${ID}"
+Suites: "${VERSION_CONDENAME}"
+Components: stable
+Architecture: $( dpkg --print-architecture )  
+Signed-by: /etc/apt/keyrings/docker.asc
+EOF
+
+	apt-cache update #&>/dev/null
+	apt-cache install docker-ce docker-ce-cli -y #&>/dev/null
+
+	printf "Docker instalado.\n"
+else
+	printf "Docker presente.\n"
+fi
+	
